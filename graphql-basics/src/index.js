b3d741d925f1a060fceb31dafd367f7b93d504e8
Guilherme Ramos <gui_aramos@outlook.comm>
2019-10-29 21:47:21 +0900

    Args and filter data with query

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,8 +1,17 @@
 import { GraphQLServer } from 'graphql-yoga'
+import Axios from 'axios'
+
+// Get user data
+const users = Axios.create({
+  baseURL: 'https://randomuser.me/api/',
+  data: 'json'
+})
 
 // 1- Type definitions
 const typeDefs = `
 type Query {
+  users(qtd: Int!, query: String):[User!]!
+  me: User!
   post: Post!
 }
 
@@ -12,10 +21,70 @@ type Post {
   body: String!
   published: Boolean!
 }
-`
 
+type User {
+  login: ILogin!
+  name: IName!
+  email: String!
+  dob: IDob!
+}
+
+type ILogin {
+  uuid: ID!
+  username: String!
+  password: String!
+  salt: String!
+  md5: ID!
+  sha1: ID!
+  sha256: ID!
+}
+
+type IDob {
+  date: String!
+  age: Int!
+}
+
+type IName {
+  title: String!
+  first: String!
+  last: String!
+}
+`
 const resolvers = {
   Query: {
+    users(obj, args, ctx, info) {
+      return users
+        .get('/', {
+          params: {
+            results: args.qtd,
+            inc: ['login', 'name', 'email', 'dob'] + ''
+          }
+        })
+        .then(res => {
+          const data = res.data.results
+          if (args.query)
+            return data.filter(user =>
+              user.name.first.toLowerCase().includes(args.query.toLowerCase())
+            )
+          return data
+        })
+    },
+    me() {
+      return {
+        id() {
+          return 'asdas798987'
+        },
+        name() {
+          return 'Guilherme Ramos'
+        },
+        email() {
+          return 'guilherme.ramos@live.com'
+        },
+        age() {
+          return 28
+        }
+      }
+    },
     post() {
       return {
         id() {
